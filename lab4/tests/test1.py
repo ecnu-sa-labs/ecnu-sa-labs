3c91d4ea1d16da3661eeb7e021e4f9cfa6adee36
ZTFan03 <ztfan1106@163.com>
2025-11-12 22:04:20 +0800

    fix(lab4): fix the English spelling; remove the icon of the makefile and the Chinese text; increase the max_examples of the hypothesis

--- lab4/tests/test1.py
+++ lab4/tests/test1.py
@@ -42,36 +42,36 @@ equivalent_trees_strategy = st.lists(
 # ---------------- Validity Testing ----------------
 
 # An empty tree is a valid binary search tree.
-def test_nil_vaild() -> None:
+def test_nil_valid() -> None:
     assert is_valid(BST.nil())
 
 
 # After inserting a key-value pair, the result remains a valid BST.
 @given(keys_strategy, st.integers(), trees_strategy)
-def test_insert_vaild(key: int, value: int, bst: BST[int,int]) -> None:
+def test_insert_valid(key: int, value: int, bst: BST[int,int]) -> None:
     # assume(is_valid(bst))
     assert is_valid(bst.insert(key, value))
 
 # After deleting a key, the result remains a valid BST.
 @given(keys_strategy, trees_strategy)
-def test_delete_vaild(key: int, bst: BST[int,int]) -> None:
+def test_delete_valid(key: int, bst: BST[int,int]) -> None:
     # assume(is_valid(bst))
     assert is_valid(bst.delete(key))
 
 # Ensure the generator is effective
 @given(trees_strategy)
-def test_strategy_vaild(bst: BST[int,int]) ->None:
+def test_strategy_valid(bst: BST[int,int]) ->None:
     assert(is_valid(bst))
 
 # TODO: After locating a key, the result remains valid in a BST.
 @given(keys_strategy, trees_strategy)
-def test_find_vaild(key: int, bst: BST[int,int]) -> None:
+def test_find_valid(key: int, bst: BST[int,int]) -> None:
     ...
 
 
 # TODO: After merging two binary search trees, the result remains a valid binary search tree.
 @given(trees_strategy, trees_strategy)
-def test_union_vaild(bst1: BST[int,int], bst2: BST[int,int]) -> None:
+def test_union_valid(bst1: BST[int,int], bst2: BST[int,int]) -> None:
     ...
 
 
