57e2718fc93388f358d6ecd9cf456aafa8876a75
ZTFan03 <ztfan1106@163.com>
2025-11-19 09:56:38 +0800

    fix(lab4): Reuse strategies

--- lab4/src/__init__.py
+++ lab4/src/__init__.py
@@ -1,5 +1,6 @@
 # bugs/__init__.py
 from .BST import BST
 from .BSTUtils import is_valid, equivalent, insertions
+from .test_strategies import create_bst_strategies
 
-__all__ = ['BST', 'is_valid', 'equivalent', 'insertions']
+__all__ = ['BST', 'is_valid', 'equivalent', 'insertions','create_bst_strategies']
